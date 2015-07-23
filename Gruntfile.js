279044496dd443aef2c5d19f6c79d284f646ea5e
Diego <ddimunzio@developementgateway.org>
2015-07-23 13:03:13 -0300

    TSD-109  -  Add TopoJson Layers and modified grunt to copy them into dist folder.

--- Gruntfile.js
+++ Gruntfile.js
@@ -516,6 +516,11 @@ module.exports = function (grunt) {
           cwd: 'bower_components/bootstrap/dist',
           src: 'fonts/*',
           dest: '<%= yeoman.dist %>'
+        }, {
+          expand: true,
+          cwd: '<%= yeoman.app %>/layers',
+          src: '**/*.json',
+          dest: '<%= yeoman.dist %>/layers'
         }]
       },
     },
